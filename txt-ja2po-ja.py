8ad87e017693cdcb225391bce1a60612b00b745d
Tetsuo Koyama <tkoyama010@gmail.com>
2018-07-14 08:56:48 +0900

    fix bug

--- txt-ja2po-ja.py
+++ txt-ja2po-ja.py
@@ -5,7 +5,7 @@ po = polib.pofile(sys.argv[2])
 
 with open(sys.argv[1]) as txt:
   for entry in po.untranslated_entries():
-    entry.msgid = txt.readline()
+    entry.msgstr = txt.readline()
 
 txt.close()
 po.save(sys.argv[2])
