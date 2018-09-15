09c4f772bc0083dd66186f36a29d18c6a310af5c
Tetsuo Koyama <tkoyama010@gmail.com>
2018-09-15 15:09:44 +0900

    update xfem

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
-for i in ../workspace/getfem/doc/sphinx/source/locale/ja/LC_MESSAGES/userdoc/xfem.po
+for i in ../getfem/doc/sphinx/source/locale/ja/LC_MESSAGES/userdoc/xfem.po
 do
   echo ${i}
   python3 po-en2txt-en.py ${i} ${i}-en.txt
-  cat ${i}-en.txt | ../translate-shell/translate -e bing en:ja -b  > ${i}-ja.txt
+  cat ${i}-en.txt | trans en:ja -b  > ${i}-ja.txt
   python3 txt-ja2po-ja.py ${i}-ja.txt ${i}
 done
