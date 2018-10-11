62b2eb7a2992798c0a79f547d62a08095d9d5a18
Tetsuo Koyama <tkoyama010@gmail.com>
2018-10-11 13:02:55 +0900

    use file interface of trans command

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,9 @@
-for i in ../getfem/doc/sphinx/source/locale/ja/LC_MESSAGES/userdoc/xfem.po
+for i in ../getfem-ja/doc/sphinx/source/locale/ja/LC_MESSAGES/project/femdesc.po
 do
   echo ${i}
   python3 po-en2txt-en.py ${i} ${i}-en.txt
 
-  rm ${i}-ja.txt
-  cat ${i}-en.txt | while read line
-  do
-    echo $line | sed -e "s/`//g" | trans en:ja -b  >> ${i}-ja.txt
-  done
+  trans en:ja -b -i ${i}-en.txt ${i}-ja.txt
 
   python3 txt-ja2po-ja.py ${i}-ja.txt ${i}
 done
