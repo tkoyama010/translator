0c6dc9e8983eb4dab013538ed29c2cd32096c088
Tetsuo Koyama <tkoyama010@gmail.com>
2018-10-11 11:35:23 +0900

    Revert "delete \n"
    This reverts commit ccd4afa776f5fadab3abf65b18bcbd1572383449.

--- po-en2txt-en.py
+++ po-en2txt-en.py
@@ -5,7 +5,7 @@ po = polib.pofile(sys.argv[1])
 
 with open(sys.argv[2], mode='w') as txt:
   for entry in po.untranslated_entries():
-    txt.writelines(entry.msgid)
+    txt.writelines(entry.msgid + '\n')
 
 txt.close()
 
