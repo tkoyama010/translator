775dd1f0a73be1033db79146cab2b45e2b32f540
Tetsuo Koyama <tkoyama010@gmail.com>
2018-10-11 13:01:28 +0900

    fix for character number limit

--- run_all.sh
+++ run_all.sh
@@ -2,6 +2,12 @@ for i in ../getfem/doc/sphinx/source/locale/ja/LC_MESSAGES/userdoc/xfem.po
 do
   echo ${i}
   python3 po-en2txt-en.py ${i} ${i}-en.txt
-  cat ${i}-en.txt | trans en:ja -b  > ${i}-ja.txt
+
+  rm ${i}-ja.txt
+  cat ${i}-en.txt | while read line
+  do
+    echo $line | sed -e "s/`//g" | trans en:ja -b  >> ${i}-ja.txt
+  done
+
   python3 txt-ja2po-ja.py ${i}-ja.txt ${i}
 done
