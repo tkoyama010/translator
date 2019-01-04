e9c065f56273c5276f03702f28a424f1f2c34d37
Tetsuo Koyama <tkoyama010@gmail.com>
2019-01-04 15:07:59 +0900

    Add TODO

--- run_all.sh
+++ run_all.sh
@@ -2,6 +2,7 @@ for i in ../getfem/doc/sphinx/source/locale/ja/LC_MESSAGES/project/libdesc_mesh.
 do
   echo ${i}
   python3 po-en2txt-en.py ${i} ${i}-en.txt
+  # TODO: 数式を連番の文字列に置換する処理を追加
 
   trans en:ja -b -i ${i}-en.txt > ${i}-ja.txt
 
