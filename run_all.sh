3e930b66dd3a2130453bcd79ba4bde14b8c58d27
Tetsuo Koyama <tkoyama010@gmail.com>
2019-01-04 15:05:49 +0900

    Update script

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,9 @@
-for i in ../getfem-ja/doc/sphinx/source/locale/ja/LC_MESSAGES/project/femdesc.po
+for i in ../getfem/doc/sphinx/source/locale/ja/LC_MESSAGES/project/libdesc_mesh.po
 do
   echo ${i}
   python3 po-en2txt-en.py ${i} ${i}-en.txt
 
-  trans en:ja -b -i ${i}-en.txt ${i}-ja.txt
+  trans en:ja -b -i ${i}-en.txt > ${i}-ja.txt
 
   python3 txt-ja2po-ja.py ${i}-ja.txt ${i}
 done
